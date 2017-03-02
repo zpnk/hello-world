abada714db4b5c1e6de69fa44f98fd70ee6a81af
Hugh A. Miles II <hmiles23@ufl.edu>
2017-03-01 21:39:09 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('express')()
 app.get('/', (req, res) => {
   const greeting = process.env.GREETING;
 
-  res.send(`${greeting || 'hello'} world 1`);
+  res.send(`${greeting || 'hello'} world 2`);
 })
 
 app.listen(process.env.PORT || 3000)
